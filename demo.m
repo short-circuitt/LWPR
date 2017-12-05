94f9702736d1a82391f0102030a4baf34af1da94
Stephanie <stephanie.lowry@oru.se>
2017-12-05 15:33:47 +0100

    Changed display output

--- demo.m
+++ demo.m
@@ -1,22 +1,14 @@
-% demo.m
+params =  defaultparameters();
 
-tpdist = 2;
-
-addpath(genpath('/home/stephanie/3rdParty/vlad_cvpr2010'));
-
-[params,dparams] =  defaultparameters();
-
-[results,results2] = matchplaces(params.datadir,dparams.dname,dparams.d1,dparams.d2,params.k,params.dim,params.fnum,params.bpw,params.pickdims);
+[results,results2] = matchplaces(params.datadir,params.dname,params.d1,params.d2,params.k,params.dim,params.fnum,params.bpw,params.pickdims);
 
 [aa,bb] = max(results,[],2);
 hdistrec = abs((1:length(bb))-bb');
-cc = sum(hdistrec<=tpdist);
-
-disp(params.pickdims)
-disp(cc)
+cc = sum(hdistrec<=params.tpdist);
 
 [aa2,bb2] = max(results2,[],2);
 hdistrec2 = abs((1:length(bb2))-bb2');
-cc2 = sum(hdistrec2<=tpdist);
+cc2 = sum(hdistrec2<=params.tpdist);
 
-disp(cc2)
+fprintf('VLAD with %i bits correctly matches %0.1f%% of places\n',params.pickdims,100*cc/length(bb));
+fprintf('BOW with %i bits correctly matches %0.1f%% of places\n',params.pickdims,100*cc2/length(bb2));
